af84862cb9f1a928f7ad6396a1e2a8fcbeb9ae13
sirzes02 <sirzes02@gmail.com>
2021-05-02 16:40:31 -0500

    profileInfo

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,28 @@
 import React from "react";
-
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from "./styles";
+import ProfileData from "../../components/ProfileData";
 
 const Profile: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username="sirzes02"
+            name="Santiago Varela"
+            avatarUrl="https://avatars.githubusercontent.com/u/33233964?v=4"
+            followers={887}
+            following={7}
+            company="USC"
+            location="Cali, Colombia"
+            email="sirzes02@gmail.com"
+            blog="sirzes02.com"
+          />
+        </LeftSide>
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  );
 };
 
 export default Profile;
