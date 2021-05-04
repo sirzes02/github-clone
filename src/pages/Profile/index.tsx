a65f0674f40c3de71c823d98fa38b6505deef109
sirzes02 <sirzes02@gmail.com>
2021-05-04 16:54:51 -0500

    tabs

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -6,14 +6,33 @@ import {
   RightSide,
   Repos,
   CalendarHeading,
+  RepoIcon,
+  Tab,
 } from "./styles";
 import ProfileData from "../../components/ProfileData";
 import RepoCard from "../../components/RepoCard";
 import RandomCalendar from "../../components/RandomCalendar";
 
 const Profile: React.FC = () => {
+  const TabContent = () => (
+    <div className="content">
+      <RepoIcon />
+      <span className="label">Repositories</span>
+      <span className="number">26</span>
+    </div>
+  );
+
   return (
     <Container>
+      <Tab className="desktop">
+        <div className="wrapper">
+          <span className="offset" />
+          <TabContent />
+        </div>
+
+        <span className="line" />
+      </Tab>
+
       <Main>
         <LeftSide>
           <ProfileData
@@ -29,6 +48,10 @@ const Profile: React.FC = () => {
           />
         </LeftSide>
         <RightSide>
+          <Tab className="mobile">
+            <TabContent />
+            <span className="line" />
+          </Tab>
           <Repos>
             <h2>Random Reports</h2>
 
