8dcb0ee9aaf7c315c98689de7bda24b7269ac1d5
sirzes02 <sirzes02@gmail.com>
2021-05-04 17:15:46 -0500

    repo

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StartIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+
+        <Link className="username" to="/sirzes02">
+          sirzes02
+        </Link>
+
+        <span>/</span>
+
+        <Link className="reponame" to="/sirzes02/youtube-content">
+          youtube-content
+        </Link>
+      </Breadcrumb>
+
+      <p>Contains all of my Youtube lessons code.</p>
+
+      <Stats>
+        <li>
+          <StartIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href="https://github.com/sirzes02/simple-portafolio">
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 };
 
 export default Repo;
