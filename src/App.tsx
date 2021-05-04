ead17e2f6a9df33c331d5a937c9f14f47b0d8b09
sirzes02 <sirzes02@gmail.com>
2021-05-04 18:01:24 -0500

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import Repo from "./pages/Repo";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
         <Route path="/:username/:reponame" element={<Repo />} />
       </Routes>
 
-      {/* <Footer /> */}
+      <Footer />
       <GlobalStyles />
     </BrowserRouter>
   );
